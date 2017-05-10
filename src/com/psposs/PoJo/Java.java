98f8d8d8b0dc292d109004cb0b4757aafeef52d5
Psposs <zhdNerver@163.com>
2017-05-10 18:07:56 +0800

    Jersey demo

---
+++ src/com/psposs/PoJo/Java.java
@@ -0,0 +1,36 @@
+package com.psposs.PoJo;
+
+/**
+ * Created by chetuan on 2017/5/10.
+ */
+public class Java {
+
+    private  String name;
+    private String sex;
+    private  String age;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+}
